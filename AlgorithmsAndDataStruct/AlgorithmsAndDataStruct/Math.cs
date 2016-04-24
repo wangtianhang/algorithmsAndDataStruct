f84d9e79e39f1846ee76eb287fdfd609742c06bf
wangtianhang <690879430@qq.com>
2016-04-24 21:06:56 +0800

    部分算法代码

---
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class MathCollection
+{
+    /// <summary>
+    /// 牛顿法求平方根
+    /// </summary>
+    /// <param name="c"></param>
+    /// <returns></returns>
+    public static double sqrt(double c)
+    {
+        if(c < 0)
+        {
+            return Double.NaN;
+        }
+
+        double err = 1e-15;
+        double t = c;
+        while(Math.Abs(t - c / t) > err * t)
+        {
+            t = (c / t + t) / 2.0;
+        }
+        return t;
+    }
+}
+
