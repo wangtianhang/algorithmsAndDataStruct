9ed830dffe140f34e4b7e28ce87adeb1b60ed506
wangtianhang <690879430@qq.com>
2016-04-25 22:52:50 +0800

    权重图

---
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/UndirectedGraph/EdgeWeightedGraph.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Edge
+{
+    int m_v;
+    int m_w;
+    double m_weight;
+
+    public Edge(int v, int w, double weight)
+    {
+        m_v = v;
+        m_w = w;
+        m_weight = weight;
+    }
+
+    public double Weight()
+    {
+        return m_weight;
+    }
+
+    public int Either()
+    {
+        return m_v;
+    }
+
+    public int Other(int vertex)
+    {
+        if(vertex == m_v)
+        {
+            return m_w;
+        }
+        else if(vertex == m_w)
+        {
+            return m_v;
+        }
+        else
+        {
+            throw new Exception("Inconsistent edge");
+        }
+    }
+
+    public int CompareTo(Edge that)
+    {
+        if(Weight() < that.Weight())
+        {
+            return -1;
+        }
+        else if(Weight() > that.Weight())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
+class EdgeWeightedGraph
+{
+    int m_v;
+    int m_e;
+    List<Edge>[] m_adj;
+
+    public EdgeWeightedGraph(int v)
+    {
+        m_v = v;
+        m_e = 0;
+        m_adj = new List<Edge>[m_v];
+        for (int i = 0; i < m_adj.Length; ++i )
+        {
+            m_adj[i] = new List<Edge>();
+        }
+    }
+
+    public int V()
+    {
+        return m_v;
+    }
+
+    public int E()
+    {
+        return m_e;
+    }
+
+    public void AddEdge(Edge e)
+    {
+        int v = e.Either();
+        int w = e.Other(v);
+        m_adj[v].Add(e);
+        m_adj[w].Add(e);
+        m_e++;
+    }
+
+    public List<Edge> Adj(int v)
+    {
+        return m_adj[v];
+    }
+}
+
