e313c2bad38a286875708f13d596ac95be94d6fa
wangtianhang <690879430@qq.com>
2016-04-25 13:32:00 +0800

    求导 gcd lcm

--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/Math.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/Math.cs
@@ -58,5 +58,71 @@ class MathCollection
 
         return a[k];
     }
+
+    /// <summary>
+    /// 求导数
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public delegate float Function(float x);
+    public static float Derivative(Function function, float x, float delta = 0.0001f)
+    {
+        return (function(x + delta) - function(x)) / delta;
+    }
+
+    /// <summary>
+    /// 最大公约数（递归），辗转相除法
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static int GCDRecursive(int a, int b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+        else
+        {
+            return GCDRecursive(b, a % b);
+        }
+    }
+
+    /// <summary>
+    /// 最大公约数，辗转相除法
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static int GCD(int a, int b)
+    {
+        int temp;          /*定义整型变量*/
+        if (a < b)             /*通过比较求出两个数中的最大值和最小值*/
+        {
+            temp = a;
+            a = b;
+            b = temp;
+        } /*设置中间变量进行两数交换*/
+        while (b != 0)           /*通过循环求两数的余数，直到余数为0*/
+        {
+            temp = a % b;
+            a = b;              /*变量数值交换*/
+            b = temp;
+        }
+        return a;            /*返回最大公约数到调用函数处*/
+    }
+
+    /// <summary>
+    /// 最小公倍数
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static int LCM(int a, int b)
+    {
+        int temp;
+        temp = GCD(a, b);  /*调用自定义函数，求出最大公约数*/
+        return (a * b / temp); /*返回最小公倍数到主调函数处进行输出*/
+    }
 }
 
