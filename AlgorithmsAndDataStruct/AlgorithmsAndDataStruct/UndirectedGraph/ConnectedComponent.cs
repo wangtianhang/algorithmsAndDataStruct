b8fdc362ec1336a19b6dddedcf792eba57d2446e
wangtianhang <690879430@qq.com>
2016-04-25 16:34:54 +0800

    检测联通分量和检测无向图是否有环

--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/UndirectedGraph/ConnectedComponent.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/UndirectedGraph/ConnectedComponent.cs
@@ -4,9 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
+/// <summary>
+/// 找到所有联通分量
+/// </summary>
 class ConnectedComponent
 {
+    bool[] m_marked;
+    int[] m_id;
+    int m_count;
 
+    public ConnectedComponent(Graph g)
+    {
+        m_marked = new bool[g.V()];
+        m_id = new int[g.V()];
+        for (int s = 0; s < g.V(); s++)
+        {
+            if(!m_marked[s])
+            {
+                Dfs(g, s);
+                m_count++;
+            }
+        }
+    }
+
+    void Dfs(Graph g, int v)
+    {
+        m_marked[v] = true;
+        m_id[v] = m_count;
+        foreach (var w in g.Adj(v))
+        {
+            if (!m_marked[w])
+            {
+                Dfs(g, w);
+            }
+        }
+    }
+
+    public bool Connected(int v, int w)
+    {
+        return m_id[v] == m_id[w];
+    }
+
+    public int Id(int v)
+    {
+        return m_id[v];
+    }
+
+    public int Count()
+    {
+        return m_count;
+    }
 }
 
--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/UndirectedGraph/Cycle.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/UndirectedGraph/Cycle.cs
@@ -4,9 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
+/// <summary>
+/// 判定是否为无环图
+/// </summary>
 class Cycle
 {
+    bool[] m_marked;
+    bool m_hasCycle;
+    public Cycle(Graph g)
+    {
+        m_marked = new bool[g.V()];
+        for (int s = 0; s < g.V(); s++ )
+        {
+            if(!m_marked[s])
+            {
+                Dfs(g, s, s);
+            }
+        }
+    }
 
+    void Dfs(Graph g, int v, int u)
+    {
+        m_marked[v] = true;
+        foreach (var w in g.Adj(v))
+        {
+            if (!m_marked[w])
+            {
+                Dfs(g, w, v);
+            }
+            else if(w != u)
+            {
+                m_hasCycle = true;
+            }
+        }
+    }
+
+    public bool HasCycle()
+    {
+        return m_hasCycle;
+    }
 }
 
