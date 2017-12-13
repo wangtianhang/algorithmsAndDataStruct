42205a80eee92c26bd7cc23d506a002cc426ece2
wangtianhang <690879430@qq.com>
2017-12-13 15:47:32 +0800

    补提交

--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/Math3d.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/Math3d.cs
@@ -17,5 +17,66 @@ class Math3d
         Vector3 intersectionPoint = rayOrigin + rayDir * t;
         return intersectionPoint;
     }
+
+    /// <summary>
+    /// 仅支持凸多边形
+    /// </summary>
+    /// <param name="p"></param>
+    /// <param name="polygon"></param>
+    /// <returns></returns>
+    public static bool IsInConvexPolygon(Vector3 p, List<Vector3> polygon)
+    {
+        if (polygon.Count < 3)
+        {
+            return false;
+        }
+
+        p.y = 0;
+        for (int i = 0; i < polygon.Count; ++i)
+        {
+            Vector3 tmp = polygon[i];
+            tmp.y = 0;
+            polygon[i] = tmp;
+        }
+
+        List<Vector3> crossResultList = new List<Vector3>();
+        for (int i = 0; i < polygon.Count; ++i)
+        {
+            if (i == polygon.Count - 1)
+            {
+                Vector3 v1 = p - polygon[i];
+                Vector3 v2 = polygon[0] - polygon[i];
+                Vector3 v3 = Vector3.Cross(v1, v2);
+                crossResultList.Add(v3);
+            }
+            else
+            {
+                Vector3 v1 = p - polygon[i];
+                Vector3 v2 = polygon[i + 1] - polygon[i];
+                Vector3 v3 = Vector3.Cross(v1, v2);
+                crossResultList.Add(v3);
+            }
+        }
+
+        for (int i = 0; i < crossResultList.Count; ++i)
+        {
+            if (i == crossResultList.Count - 1)
+            {
+                if (crossResultList[i].y * crossResultList[0].y < 0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (crossResultList[i].y * crossResultList[i + 1].y < 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
 
