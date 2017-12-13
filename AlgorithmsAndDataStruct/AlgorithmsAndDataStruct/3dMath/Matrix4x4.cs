966c9c8252cdf11e6a7148d8aba0a2ee1d16a49e
wangtianhang <690879430@qq.com>
2017-12-13 13:51:15 +0800

    添加部分3d数学模块

---
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/Matrix4x4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Matrix4x4
+{
+
+}
+
---
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/Transformcs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Transformcs
+{
+    Vector3 m_position;
+
+    //Vector3 m_localScale;
+
+    Quaternion m_rotation;
+}
+
