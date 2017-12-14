d0a88bd759bdfad9b8ea4aed338d02927de48451
wangtianhang <690879430@qq.com>
2017-12-14 14:38:11 +0800

    部分mathf代码

--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/Mathf.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/Mathf.cs
@@ -12,6 +12,53 @@ public struct Mathf
     public const float PI = 3.14159f;
     public const float Rad2Deg = 57.2958f;
 
+
+
+
+
+    public static float Abs(float f)
+    {
+        return Math.Abs(f);
+    }
+    public static int Abs(int value)
+    {
+        return Math.Abs(value);
+    }
+
+    public static float Acos(float f)
+    {
+        return (float)Math.Acos((double)f);
+    }
+
+    public static bool Approximately(float a, float b)
+    {
+        return Mathf.Abs(b - a) < Mathf.Max(1E-06f * Mathf.Max(Mathf.Abs(a), Mathf.Abs(b)), 1.121039E-44f);
+    }
+    public static float Asin(float f)
+    {
+        return (float)Math.Asin((double)f);
+    }
+
+    public static float Atan(float f)
+    {
+        return (float)Math.Atan((double)f);
+    }
+
+    public static float Atan2(float y, float x)
+    {
+        return (float)Math.Atan2((double)y, (double)x);
+    }
+
+    public static float Ceil(float f)
+    {
+        return (float)Math.Ceiling((double)f);
+    }
+
+    public static int CeilToInt(float f)
+    {
+        return (int)Math.Ceiling((double)f);
+    }
+
     public static float Clamp(float value, float min, float max)
     {
         if (value < min)
@@ -51,28 +98,115 @@ public struct Mathf
         return value;
     }
 
+    public static float Cos(float f)
+    {
+        return (float)Math.Cos((double)f);
+    }
+
+    public static float DeltaAngle(float current, float target)
+    {
+        float num = Mathf.Repeat(target - current, 360f);
+        if (num > 180f)
+        {
+            num -= 360f;
+        }
+        return num;
+    }
+
+    public static float Exp(float power)
+    {
+        return (float)Math.Exp((double)power);
+    }
+
+    public static float Floor(float f)
+    {
+        return (float)Math.Floor((double)f);
+    }
+
+    public static int FloorToInt(float f)
+    {
+        return (int)Math.Floor((double)f);
+    }
+
+    public static float Gamma(float value, float absmax, float gamma)
+    {
+        bool flag = false;
+        if (value < 0f)
+        {
+            flag = true;
+        }
+        float num = Mathf.Abs(value);
+        if (num > absmax)
+        {
+            return (!flag) ? num : (-num);
+        }
+        float num2 = Mathf.Pow(num / absmax, gamma) * absmax;
+        return (!flag) ? num2 : (-num2);
+    }
+
+    public static float InverseLerp(float from, float to, float value)
+    {
+        if (from < to)
+        {
+            if (value < from)
+            {
+                return 0f;
+            }
+            if (value > to)
+            {
+                return 1f;
+            }
+            value -= from;
+            value /= to - from;
+            return value;
+        }
+        else
+        {
+            if (from <= to)
+            {
+                return 0f;
+            }
+            if (value < to)
+            {
+                return 1f;
+            }
+            if (value > from)
+            {
+                return 0f;
+            }
+            return 1f - (value - to) / (from - to);
+        }
+    }
+
+
     public static float Lerp(float from, float to, float t)
     {
         return from + (to - from) * Mathf.Clamp01(t);
     }
 
-    public static float Abs(float f)
+    public static float LerpAngle(float a, float b, float t)
     {
-        return Math.Abs(f);
-    }
-    public static int Abs(int value)
-    {
-        return Math.Abs(value);
-    }
-
-    public static float Acos(float f)
-    {
-        return (float)Math.Acos((double)f);
+        float num = Mathf.Repeat(b - a, 360f);
+        if (num > 180f)
+        {
+            num -= 360f;
+        }
+        return a + num * Mathf.Clamp01(t);
     }
 
-    public static bool Approximately(float a, float b)
+    public static float Log(float f)
     {
-        return Mathf.Abs(b - a) < Mathf.Max(1E-06f * Mathf.Max(Mathf.Abs(a), Mathf.Abs(b)), 1.121039E-44f);
+        return (float)Math.Log((double)f);
+    }
+
+    public static float Log(float f, float p)
+    {
+        return (float)Math.Log((double)f, (double)p);
+    }
+
+    public static float Log10(float f)
+    {
+        return (float)Math.Log10((double)f);
     }
 
     public static float Max(float a, float b)
@@ -165,6 +299,102 @@ public struct Mathf
             }
         }
         return num2;
+    }
+
+    public static float MoveTowards(float current, float target, float maxDelta)
+    {
+        if (Mathf.Abs(target - current) <= maxDelta)
+        {
+            return target;
+        }
+        return current + Mathf.Sign(target - current) * maxDelta;
+    }
+
+    public static float MoveTowardsAngle(float current, float target, float maxDelta)
+    {
+        target = current + Mathf.DeltaAngle(current, target);
+        return Mathf.MoveTowards(current, target, maxDelta);
+    }
+
+    public static float PingPong(float t, float length)
+    {
+        t = Mathf.Repeat(t, length * 2f);
+        return length - Mathf.Abs(t - length);
+    }
+
+    public static float Pow(float f, float p)
+    {
+        return (float)Math.Pow((double)f, (double)p);
+    }
+
+    public static float Repeat(float t, float length)
+    {
+        return t - Mathf.Floor(t / length) * length;
+    }
+
+    public static float Round(float f)
+    {
+        return (float)Math.Round((double)f);
+    }
+
+    public static int RoundToInt(float f)
+    {
+        return (int)Math.Round((double)f);
+    }
+
+    public static float Sign(float f)
+    {
+        return (f < 0f) ? -1f : 1f;
+    }
+
+    public static float Sin(float f)
+    {
+        return (float)Math.Sin((double)f);
+    }
+
+    public static float SmoothDamp(float current, float target, ref float currentVelocity, float smoothTime, float maxSpeed, float deltaTime)
+    {
+        smoothTime = Mathf.Max(0.0001f, smoothTime);
+        float num = 2f / smoothTime;
+        float num2 = num * deltaTime;
+        float num3 = 1f / (1f + num2 + 0.48f * num2 * num2 + 0.235f * num2 * num2 * num2);
+        float num4 = current - target;
+        float num5 = target;
+        float num6 = maxSpeed * smoothTime;
+        num4 = Mathf.Clamp(num4, -num6, num6);
+        target = current - num4;
+        float num7 = (currentVelocity + num * num4) * deltaTime;
+        currentVelocity = (currentVelocity - num * num7) * num3;
+        float num8 = target + (num4 + num7) * num3;
+        if (num5 - current > 0f == num8 > num5)
+        {
+            num8 = num5;
+            currentVelocity = (num8 - num5) / deltaTime;
+        }
+        return num8;
+    }
+
+    public static float SmoothDampAngle(float current, float target, ref float currentVelocity, float smoothTime, float maxSpeed, float deltaTime)
+    {
+        target = current + Mathf.DeltaAngle(current, target);
+        return Mathf.SmoothDamp(current, target, ref currentVelocity, smoothTime, maxSpeed, deltaTime);
+    }
+
+    public static float SmoothStep(float from, float to, float t)
+    {
+        t = Mathf.Clamp01(t);
+        t = -2f * t * t * t + 3f * t * t;
+        return to * t + from * (1f - t);
+    }
+
+    public static float Sqrt(float f)
+    {
+        return (float)Math.Sqrt((double)f);
+    }
+
+    public static float Tan(float f)
+    {
+        return (float)Math.Tan((double)f);
     }
 }
 
