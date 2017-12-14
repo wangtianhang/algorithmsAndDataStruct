8286fffbde89dd236574dcda40af56df91a8e6df
wangtianhang <690879430@qq.com>
2017-12-14 22:07:24 +0800

    补提交

--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/Other/Tuple.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/Other/Tuple.cs
@@ -104,7 +104,7 @@ public class Tuple<T1, T2> : IComparable<Tuple<T1, T2>>
 
     public override Boolean Equals(Object obj)
     {
-        Tuple<T1, T2> other = (Tuple<T1, T2>)obj;
+        var other = (Tuple<T1, T2>)obj;
         return Item1.CompareTo(other.Item1) == 0
             && Item2.CompareTo(other.Item2) == 0;
     }
@@ -142,6 +142,80 @@ public class Tuple<T1, T2> : IComparable<Tuple<T1, T2>>
                     return Item1;
                 case 1:
                     return Item2;
+                default:
+                    throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
+public class Tuple<T1, T2, T3> : IComparable<Tuple<T1, T2, T3>>
+    where T1 : IComparable<T1>
+    where T2 : IComparable<T2>
+    where T3 : IComparable<T3>
+{
+    private readonly T1 m_Item1;
+    private readonly T2 m_Item2;
+    private readonly T3 m_Item3;
+
+    public T1 Item1 { get { return m_Item1; } }
+    public T2 Item2 { get { return m_Item2; } }
+    public T3 Item3 { get { return m_Item3; } }
+
+    public Tuple(T1 item1, T2 item2, T3 item3)
+    {
+        m_Item1 = item1;
+        m_Item2 = item2;
+        m_Item3 = item3;
+    }
+
+    public override Boolean Equals(Object obj)
+    {
+        var other = (Tuple<T1, T2, T3>)obj;
+        return Item1.CompareTo(other.Item1) == 0
+            && Item2.CompareTo(other.Item2) == 0
+            && Item3.CompareTo(other.Item3) == 0;
+    }
+
+    public Int32 CompareTo(Tuple<T1, T2, T3> obj)
+    {
+        int c = Item1.CompareTo(obj.Item1);
+        if (c != 0)
+        {
+            return c;
+        }
+
+        c = Item2.CompareTo(obj.Item2);
+        if(c != 0)
+        {
+            return c;
+        }
+        return Item3.CompareTo(obj.m_Item3);
+    }
+
+    public override int GetHashCode()
+    {
+        return Tuple.CombineHashCodes(m_Item1.GetHashCode(), m_Item2.GetHashCode(), m_Item3.GetHashCode());
+    }
+
+    public override string ToString()
+    {
+        return "Tuple " + m_Item1.ToString() + " " + m_Item2.ToString() + " " + m_Item3.ToString();
+    }
+
+    public int Length = 3;
+
+    object this[int index]
+    {
+        get
+        {
+            switch (index)
+            {
+                case 0:
+                    return Item1;
+                case 1:
+                    return Item2;
+                case 2:
+                    return Item3;
                 default:
                     throw new IndexOutOfRangeException();
             }
