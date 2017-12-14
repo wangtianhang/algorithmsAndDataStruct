f6cc78460df227caf2011b6be36211f7ce380257
wangtianhang <690879430@qq.com>
2017-12-14 23:11:37 +0800

    补提交

--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/CommonAlgorithms/MathCollection.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/CommonAlgorithms/MathCollection.cs
@@ -7,6 +7,19 @@ using System.Text;
 
 class MathCollection
 {
+    public static void Test()
+    {
+        string tmp = TenToTwo(10);
+        Debug.Log(tmp);
+        Debug.Log(TwoToTen(tmp).ToString());
+
+        string tmp2 = TenToSixteen(10);
+        Debug.Log(tmp2);
+        Debug.Log(SixteenToTen(tmp2).ToString());
+
+        Console.ReadLine();
+    }
+
     /// <summary>
     /// 牛顿法求平方根
     /// </summary>
@@ -172,6 +185,42 @@ class MathCollection
             ret.Add(x);
         }
         return ret;
+    }
+
+    /// <summary>
+    /// 十进制转二进制
+    /// </summary>
+    /// <returns></returns>
+    public static string TenToTwo(int x)
+    {
+        return System.Convert.ToString(x, 2);
+    }
+
+    /// <summary>
+    /// 二进制转十进制
+    /// </summary>
+    /// <returns></returns>
+    public static int TwoToTen(string s)
+    {
+        return System.Convert.ToInt32(s, 2);
+    }
+
+    /// <summary>
+    /// 十进制转十六进制
+    /// </summary>
+    /// <returns></returns>
+    public static string TenToSixteen(int x)
+    {
+        return string.Format("{0:X}", x);
+    }
+
+    /// <summary>
+    /// 十六进制转十进制
+    /// </summary>
+    /// <returns></returns>
+    public static int SixteenToTen(string s)
+    {
+        return System.Convert.ToInt32(s, 16);
     }
 }
 
--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/Program.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/Program.cs
@@ -11,7 +11,7 @@ namespace AlgorithmsAndDataStruct
         static void Main(string[] args)
         {
             //Point24.Test();
-            Math3d.Test();
+            MathCollection.Test();
             
             //Matrix4x4.Test();
         }
