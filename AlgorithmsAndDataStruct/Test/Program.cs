6d33c35b6438cb693375ef3670cbdc087f117c1e
wangtianhang <690879430@qq.com>
2017-12-15 19:00:00 +0800

    工程调整

--- AlgorithmsAndDataStruct/Test/Program.cs
+++ AlgorithmsAndDataStruct/Test/Program.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Numerics;
-using Numerics;
+
 
 class Program
 {
     static void Main(string[] args)
     {
         BigMath.Test();
+
+        AlgorithmsBase.Test();
 
         Console.ReadLine();
     }
