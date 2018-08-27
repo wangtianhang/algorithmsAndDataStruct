55a0be0375fb0f1eda92c272459433f979b29a09
wangtianhang <690879430@qq.com>
2018-08-27 14:27:44 +0800

    补提交

--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/intersectionTest/IntersectionTest3D.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/intersectionTest/IntersectionTest3D.cs
@@ -1170,6 +1170,62 @@ public class IntersectionTest3D
 	    return false;
     }
 
+    public static bool Mesh3dWithSphere3d(Mesh3d mesh, Sphere3d sphere)
+    {
+        if (mesh.m_accelerator == null)
+        {
+            for (int i = 0; i < mesh.m_triangleList.Count; ++i)
+            {
+                if (Triangle3dWithSphere3d(mesh.m_triangleList[i], sphere))
+                {
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            //std::list<BVHNode*> toProcess;
+            //toProcess.push_front(mesh.accelerator);
+            List<Mesh3d.BVHNode> toProcess = new List<Mesh3d.BVHNode>();
+            toProcess.Add(mesh.m_accelerator);
+
+            // Recursivley walk the BVH tree
+            while (toProcess.Count != 0)
+            {
+                //BVHNode* iterator = *(toProcess.begin());
+                //toProcess.erase(toProcess.begin());
+                Mesh3d.BVHNode iterator = toProcess[0];
+                toProcess.RemoveAt(0);
+
+                if (iterator.m_triangles.Count >= 0)
+                {
+                    // Iterate trough all triangles of the node
+                    for (int i = 0; i < iterator.m_triangles.Count; ++i)
+                    {
+                        // Triangle indices in BVHNode index the mesh
+                        if (Triangle3dWithSphere3d(iterator.m_triangles[i], sphere))
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                if (iterator.m_children != null)
+                {
+                    for (int i = 8 - 1; i >= 0; --i)
+                    {
+                        // Only push children whos bounds intersect the test geometry
+                        if (Sphere3dWithAABB3d(sphere, iterator.m_children[i].m_bounds))
+                        {
+                            toProcess.Insert(0, iterator.m_children[i]);
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
     public static float Ray3dWithModel3d(Ray3d ray, Model3d model)
     {
         Matrix4x4 obj2world = model.GetObj2WorldMatrix();
@@ -1193,6 +1249,19 @@ public class IntersectionTest3D
         if(model.GetMesh() != null)
         {
             return Segment3dWithMesh3d(model.GetMesh(), segment);
+        }
+        return false;
+    }
+
+    public static bool Model3dWithSphere3d(Model3d model, Sphere3d sphere)
+    {
+        Matrix4x4 obj2world = model.GetObj2WorldMatrix();
+        Matrix4x4 world2Obj = obj2world.inverse;
+        Sphere3d local = new Sphere3d(world2Obj * sphere.m_pos, sphere.m_radius);
+        //local.position = MultiplyPoint(sphere.position, inv);
+        if (model.GetMesh() != null)
+        {
+            return Mesh3dWithSphere3d(model.GetMesh(), local);
         }
         return false;
     }
--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/shape3D/Sphere3d.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/shape3D/Sphere3d.cs
@@ -5,6 +5,11 @@ using System.Text;
 
 public class Sphere3d
 {
+    public Sphere3d(Vector3 pos, float radius)
+    {
+        m_pos = pos;
+        m_radius = radius;
+    }
     public Vector3 m_pos;
     public float m_radius;
 }
