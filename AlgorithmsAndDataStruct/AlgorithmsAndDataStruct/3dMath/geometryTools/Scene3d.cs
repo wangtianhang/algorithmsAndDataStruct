0c5518495f4335b79628301475c9bd3b7d03ec1b
wth <690879430@qq.com>
2018-08-27 22:23:47 +0800

    部分场景model代码

--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/geometryTools/Scene3d.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/geometryTools/Scene3d.cs
@@ -38,5 +38,47 @@ public class Scene3d
         }
         return ret;
     }
+
+    public Model3d Raycast(Ray3d ray)
+    {
+        Model3d result = null;
+        float result_t = -1;
+
+        for (int i = 0, size = m_modleList.Count; i < size; ++i)
+        {
+            float t = IntersectionTest3D.Ray3dWithModel3d(ray, m_modleList[i]);
+            if (result == null && t >= 0)
+            {
+                result = m_modleList[i];
+                result_t = t;
+            }
+            else if (result != null && t < result_t)
+            {
+                result = m_modleList[i];
+                result_t = t;
+            }
+        }
+
+        return result;
+    }
+
+    public List<Model3d> Query(Sphere3d sphere)
+    {
+        List<Model3d> result = new List<Model3d>();
+        for (int i = 0, size = m_modleList.Count; i < size; ++i)
+        {
+            OBB3d bounds = m_modleList[i].GetOBB();
+            if (IntersectionTest3D.Sphere3dWithObb3d(sphere, bounds))
+            {
+                result.Add(m_modleList[i]);
+            }
+        }
+        return result;
+    }
+
+//     public List<Model3d> Query(AABB3d aabb)
+//     {
+//         return null;
+//     }
 }
 
--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/shape3D/Model3d.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/shape3D/Model3d.cs
@@ -11,6 +11,44 @@ public class Model3d
     public Model3d m_parent = null;
 
     Mesh3d m_mesh3d;
+    AABB3d m_aabb;
+
+    public void SetContent(Mesh3d mesh)
+    {
+        m_mesh3d = mesh;
+
+        Vector3[] vertices = mesh.GetVertices();
+        Vector3 min = vertices[0];
+        Vector3 max = vertices[0];
+
+        for (int i = 1; i < mesh.m_triangleList.Count * 3; ++i)
+        {
+            min.x = Mathf.Min(vertices[i].x, min.x);
+            min.y = Mathf.Min(vertices[i].y, min.y);
+            min.z = Mathf.Min(vertices[i].z, min.z);
+
+            max.x = Mathf.Max(vertices[i].x, max.x);
+            max.y = Mathf.Max(vertices[i].y, max.y);
+            max.z = Mathf.Max(vertices[i].z, max.z);
+        }
+        m_aabb = Mesh3d.FromMinMax(min, max);
+    }
+
+    /// <summary>
+    /// 无位移 无旋转 原始aabb
+    /// </summary>
+    /// <returns></returns>
+    public AABB3d GetOriginAABB()
+    {
+        return m_aabb;
+    }
+
+    public OBB3d GetOBB()
+    {
+        Matrix4x4 obj2World = GetObj2WorldMatrix();
+        OBB3d obb = new OBB3d(obj2World * m_aabb.m_pos, m_rotation, m_aabb.m_xLength, m_aabb.m_yLength, m_aabb.m_zLength);
+        return obb;
+    }
 
     public Mesh3d GetMesh()
     {
