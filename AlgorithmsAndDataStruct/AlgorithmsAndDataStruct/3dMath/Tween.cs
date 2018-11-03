e2b56fcda7bf34a8ad07e8325edec7b81958bf11
wth <690879430@qq.com>
2018-11-03 21:31:27 +0800

    添加tween算法

---
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/3dMath/Tween.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Tween
+{
+    public enum Method
+    {
+        Linear,
+        EaseIn, // 慢速开始
+        EaseOut, // 慢速结束
+        EaseInOut,
+    }
+
+    public static float Sample(float factor, Method method)
+    {
+        float val = Mathf.Clamp01(factor);
+
+        if (method == Method.EaseIn)
+        {
+            val = 1f - Mathf.Sin(0.5f * Mathf.PI * (1f - val));
+        }
+        else if (method == Method.EaseOut)
+        {
+            val = Mathf.Sin(0.5f * Mathf.PI * val);
+        }
+        else if (method == Method.EaseInOut)
+        {
+            const float pi2 = Mathf.PI * 2f;
+            val = val - Mathf.Sin(val * pi2) / pi2;
+        }
+
+        return val;
+    }
+
+    public static Vector3 CurvePos(float factor, Method method, Vector3 srcPos, Vector3 desPos)
+    {
+        float factorNew = Sample(factor, method);
+        Vector3 curPos = srcPos * (1f - factorNew) + desPos * factorNew;
+        return curPos;
+    }
+
+    public static Quaternion CurveQua(float factor, Method method, Quaternion srcQua, Quaternion desQua)
+    {
+        float factorNew = Sample(factor, method);
+        return Quaternion.Slerp(srcQua, desQua, factorNew);
+    }
+}
+
