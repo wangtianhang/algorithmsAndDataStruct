d4906a59c6f0de8ac25c068706864f0a3a239481
wth <690879430@qq.com>
2018-11-03 23:06:08 +0800

    补充search和sort

---
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/SortAndSearch/SearchOther.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class SearchOther
+{
+    public static int LinearSearch(int[] arr, int n, int x)
+    {
+        for (int i = 0; i < n; i++) 
+        { 
+            // Return the index of the element if the element 
+            // is found 
+            if (arr[i] == x) 
+                return i; 
+        } 
+   
+        // return -1 if the element is not found 
+        return -1; 
+    }
+
+    /// <summary>
+    /// Jump Search
+    /// 适用于回溯成本很高的数据集
+    /// Binary Search is better than Jump Search, but Jump search has an advantage that we traverse back only once (Binary Search may require up to O(Log n) jumps, consider a situation where the element to be search is the smallest element or smaller than the smallest). So in a systems where jumping back is costly, we use Jump Search.
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public static int jumpSearch(int[] arr, int x)
+    {
+        int n = arr.Length;
+
+        // Finding block size to be jumped 
+        int step = (int)Math.Floor(Math.Sqrt(n));
+
+        // Finding the block where element is 
+        // present (if it is present) 
+        int prev = 0;
+        while (arr[Math.Min(step, n) - 1] < x)
+        {
+            prev = step;
+            step += (int)Math.Floor(Math.Sqrt(n));
+            if (prev >= n)
+                return -1;
+        }
+
+        // Doing a linear search for x in block 
+        // beginning with prev. 
+        while (arr[prev] < x)
+        {
+            prev++;
+
+            // If we reached next block or end of 
+            // array, element is not present. 
+            if (prev == Math.Min(step, n))
+                return -1;
+        }
+
+        // If element is found 
+        if (arr[prev] == x)
+            return prev;
+
+        return -1;
+    }
+
+    /// <summary>
+    /// 适用于均匀分布的数据集，这种数据集上速度超过二分法
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    static int interpolationSearch(int[] arr, int x)
+    {
+        // Find indexes of 
+        // two corners 
+        int lo = 0, hi = (arr.Length - 1);
+
+        // Since array is sorted,  
+        // an element present in 
+        // array must be in range 
+        // defined by corner 
+        while (lo <= hi &&
+                x >= arr[lo] &&
+                x <= arr[hi])
+        {
+            // Probing the position  
+            // with keeping uniform  
+            // distribution in mind. 
+            int pos = lo + (((hi - lo) /
+                             (arr[hi] - arr[lo])) *
+                                   (x - arr[lo]));
+
+            // Condition of  
+            // target found 
+            if (arr[pos] == x)
+                return pos;
+
+            // If x is larger, x 
+            // is in upper part 
+            if (arr[pos] < x)
+                lo = pos + 1;
+
+            // If x is smaller, x  
+            // is in the lower part 
+            else
+                hi = pos - 1;
+        }
+        return -1;
+    }
+
+    // 适用于超级大，没有头的数据集
+    //Exponential Binary Search is particularly useful for unbounded searches, where size of array is infinite.
+    static int exponentialSearch(int[] arr,
+                         int n, int x)
+    {
+
+        // If x is present at  
+        // first location itself 
+        if (arr[0] == x)
+            return 0;
+
+        // Find range for binary search  
+        // by repeated doubling 
+        int i = 1;
+        while (i < n && arr[i] <= x)
+            i = i * 2;
+
+        // Call binary search for 
+        // the found range. 
+        return binarySearch(arr, i / 2,
+                           Math.Min(i, n), x);
+    }
+
+    static int binarySearch(int[] arr, int l,
+                        int r, int x)
+    {
+        if (r >= l)
+        {
+            int mid = l + (r - l) / 2;
+
+            // If the element is present 
+            // at the middle itself 
+            if (arr[mid] == x)
+                return mid;
+
+            // If element is smaller than 
+            // mid, then it can only be  
+            // present n left subarray 
+            if (arr[mid] > x)
+                return binarySearch(arr, l, mid - 1, x);
+
+            // Else the element can only  
+            // be present in right subarray 
+            return binarySearch(arr, mid + 1, r, x);
+        }
+
+        // We reach here when element 
+        // is not present in array 
+        return -1;
+    } 
+}
+
---
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/SortAndSearch/SortOther.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class SortOther
+{
+    //The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning. 
+    static void SelectionSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        // One by one move boundary of unsorted subarray 
+        for (int i = 0; i < n - 1; i++)
+        {
+            // Find the minimum element in unsorted array 
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first 
+            // element 
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    /// <summary>
+    /// 冒泡法
+    /// </summary>
+    /// <param name="arr"></param>
+    static void bubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1])
+                {
+                    // swap temp and arr[i] 
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+
+    /// <summary>
+    /// 插入法 时间复杂度 O^2
+    /// </summary>
+    /// <param name="arr"></param>
+    void InsertSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 1; i < n; ++i)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1], 
+            // that are greater than key,  
+            // to one position ahead of 
+            // their current position 
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    #region MergeSort
+    // Merges two subarrays of arr[]. 
+    // First subarray is arr[l..m] 
+    // Second subarray is arr[m+1..r] 
+    void merge(int[] arr, int l, int m, int r) 
+    { 
+        // Find sizes of two subarrays to be merged 
+        int n1 = m - l + 1; 
+        int n2 = r - m; 
+  
+        /* Create temp arrays */
+        int[] L = new int [n1]; 
+        int[] R = new int [n2]; 
+  
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+
+        {
+            /* Merge the temp arrays */
+
+            // Initial indexes of first and second subarrays 
+            int i = 0, j = 0;
+
+            // Initial index of merged subarry array 
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            /* Copy remaining elements of L[] if any */
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            /* Copy remaining elements of R[] if any */
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            } 
+        }
+
+    } 
+
+    // Main function that sorts arr[l..r] using 
+    // merge() 
+    // 合并排序 最好 最坏 平均 时间复杂度都是nLogn
+    void MergeSort(int[] arr, int l, int r) 
+    { 
+        if (l < r) 
+        { 
+            // Find the middle point 
+            int m = (l+r)/2; 
+  
+            // Sort first and second halves 
+            MergeSort(arr, l, m); 
+            MergeSort(arr , m+1, r); 
+  
+            // Merge the sorted halves 
+            merge(arr, l, m, r); 
+        } 
+    }
+    #endregion
+
+    #region HeapSort
+    /// <summary>
+    /// 时间复杂度  O(Logn)
+    /// Heap sort algorithm has limited uses because Quicksort and Mergesort are better in practice. 
+    /// </summary>
+    /// <param name="arr"></param>
+    public void HeapSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        // Build heap (rearrange array) 
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap 
+        for (int i = n - 1; i >= 0; i--)
+        {
+            // Move current root to end 
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap 
+            heapify(arr, i, 0);
+        }
+    }
+
+    // To heapify a subtree rooted with node i which is 
+    // an index in arr[]. n is size of heap 
+    void heapify(int[] arr, int n, int i)
+    {
+        int largest = i; // Initialize largest as root 
+        int l = 2 * i + 1; // left = 2*i + 1 
+        int r = 2 * i + 2; // right = 2*i + 2 
+
+        // If left child is larger than root 
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far 
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root 
+        if (largest != i)
+        {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree 
+            heapify(arr, n, largest);
+        }
+    }
+
+    #endregion
+
+    #region QuickSort
+    /* This function takes last element as pivot, 
+    places the pivot element at its correct 
+    position in sorted array, and places all 
+    smaller (smaller than pivot) to left of 
+    pivot and all greater elements to right 
+    of pivot */
+    static int partition(int[] arr, int low,
+                                   int high)
+    {
+        int pivot = arr[high];
+
+        // index of smaller element 
+        int i = (low - 1);
+        for (int j = low; j < high; j++)
+        {
+            // If current element is smaller  
+            // than or equal to pivot 
+            if (arr[j] <= pivot)
+            {
+                i++;
+
+                // swap arr[i] and arr[j] 
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot) 
+        int temp1 = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp1;
+
+        return i + 1;
+    }
+
+
+    /* The main function that implements QuickSort() 
+    arr[] --> Array to be sorted, 
+    low --> Starting index, 
+    high --> Ending index */
+    // 不稳定排序（但是可以被调整为稳定算法by index） 最坏情况时间复杂度O^2
+    /*Why Quick Sort is preferred over MergeSort for sorting Arrays
+Quick Sort in its general form is an in-place sort (i.e. it doesn’t require any extra storage) whereas merge sort requires O(N) extra storage, N denoting the array size which may be quite expensive. Allocating and de-allocating the extra space used for merge sort increases the running time of the algorithm. Comparing average complexity we find that both type of sorts have O(NlogN) average complexity but the constants differ. For arrays, merge sort loses due to the use of extra O(N) storage space.
+
+Most practical implementations of Quick Sort use randomized version. The randomized version has expected time complexity of O(nLogn). The worst case is possible in randomized version also, but worst case doesn’t occur for a particular pattern (like sorted array) and randomized Quick Sort works well in practice.
+
+Quick Sort is also a cache friendly sorting algorithm as it has good locality of reference when used for arrays.
+
+Quick Sort is also tail recursive, therefore tail call optimizations is done.
+     */
+
+    /*
+    Why MergeSort is preferred over QuickSort for Linked Lists?
+    In case of linked lists the case is different mainly due to difference in memory allocation of arrays and linked lists. Unlike arrays, linked list nodes may not be adjacent in memory. Unlike array, in linked list, we can insert items in the middle in O(1) extra space and O(1) time. Therefore merge operation of merge sort can be implemented without extra space for linked lists.
+In arrays, we can do random access as elements are continuous in memory. Let us say we have an integer (4-byte) array A and let the address of A[0] be x then to access A[i], we can directly access the memory at (x + i*4). Unlike arrays, we can not do random access in linked list. Quick Sort requires a lot of this kind of access. In linked list to access i’th index, we have to travel each and every node from the head to i’th node as we don’t have continuous block of memory. Therefore, the overhead increases for quick sort. Merge sort accesses data sequentially and the need of random access is low.
+     */
+    static void quickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+
+            /* pi is partitioning index, arr[pi] is  
+            now at right place */
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before 
+            // partition and after partition 
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    } 
+    #endregion
+}
+
