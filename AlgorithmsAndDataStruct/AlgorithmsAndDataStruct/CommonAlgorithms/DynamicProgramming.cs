7437baa52d799a6a66273f1ce3cdc554017b2d00
wangtianhang <690879430@qq.com>
2018-11-06 17:24:59 +0800

    卡特兰数

--- AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/CommonAlgorithms/DynamicProgramming.cs
+++ AlgorithmsAndDataStruct/AlgorithmsAndDataStruct/CommonAlgorithms/DynamicProgramming.cs
@@ -289,5 +289,30 @@ public class DynamicProgramming
         }
         return maxsum;
     }
+
+    /// <summary>
+    /// 卡特兰数
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static long CatlanDP(int n)
+    {
+        // Table to store results of subproblems 
+        long[] catalan = new long[n + 1]; 
+  
+        // Initialize first two values in table 
+        catalan[0] = catalan[1] = 1; 
+  
+        // Fill entries in catalan[] using recursive formula 
+        for (int i=2; i<=n; i++) 
+        { 
+            catalan[i] = 0; 
+            for (int j=0; j<i; j++) 
+                catalan[i] += catalan[j] * catalan[i-j-1]; 
+        } 
+  
+        // Return last entry 
+        return catalan[n]; 
+    }
 }
 
